b2a56c8510d7439cadbde70e9fa6a8eb28d1e812
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-02-09 00:49:11 -0300

    chore: add navigation links constants

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,16 @@
+export const navigationLinks = [
+  {
+    href: '/pokedex',
+    label: 'Pokedex',
+  },
+
+  {
+    href: '/sign-in',
+    label: 'Login',
+  },
+
+  {
+    href: '/sign-up',
+    label: 'Cadastro',
+  },
+];
