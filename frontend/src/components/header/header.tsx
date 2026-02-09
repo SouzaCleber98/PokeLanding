14205e468107879a2cb76712a8ed8bff73b0b306
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-02-09 13:32:50 -0300

    refactor: update Header component styles and improve layout
    Removed the 'use client' directive from the Header component to make it a Server Component.
    Updated layout and styling adjustments to improve structure and maintain consistency.

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { cn } from '@/lib/utils';
 
 import Image from 'next/image';
@@ -8,17 +6,17 @@ import Navbar from './navbar';
 
 export default function Header() {
   return (
-    <header className={'flex sticky justify-between p-3 bg-yellow-400 '}>
+    <header className='flex sticky justify-between p-3 md:px-10 bg-yellow-400 shadow-sm border-b-2 border-b-amber-400'>
       <Link className='flex gap-2 items-center' href='/'>
         <Image
           src='/images/logo.png'
           alt='logo'
-          width={40}
-          height={40}
-          className='ease-in duration-300 hover:rotate-360 hover:scale-110'
+          width={60}
+          height={60}
+          className='ease-in duration-300 hover:rotate-360 hover:scale-110 '
         />
 
-        <p className='hidden sm:block text-xl font-extrabold '>
+        <p className='hidden sm:block text-2xl font-extrabold '>
           Poké<span className='text-red-600'>Landing</span>
         </p>
       </Link>
