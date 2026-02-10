a2f43b98075da35c64247787c170039b6f53cf68
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-02-10 18:47:53 -0300

    style: update Header component for improved layout and responsiveness

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -6,7 +6,7 @@ import Navbar from './navbar';
 
 export default function Header() {
   return (
-    <header className='flex sticky justify-between p-3 md:px-10 bg-yellow-300 shadow-sm border-b-2 border-b-yellow-400 z-1'>
+    <header className='flex sticky top-0 justify-between p-3 md:px-10 bg-yellow-300 shadow-sm border-b-2 border-b-yellow-400 z-1'>
       <Link className='flex gap-2 items-center' href='/'>
         <Image
           src='/images/logo.png'
@@ -16,7 +16,7 @@ export default function Header() {
           className='ease-in duration-300 hover:rotate-360 hover:scale-110 '
         />
 
-        <p className='hidden sm:block text-2xl font-extrabold '>
+        <p className='text-2xl sm:text-3xl md:text-4xl font-extrabold '>
           Poké<span className='text-red-600'>Landing</span>
         </p>
       </Link>
