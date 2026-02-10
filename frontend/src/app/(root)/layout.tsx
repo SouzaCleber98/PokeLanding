3dbfa632794050575daebfeda8d01ea037aa3fc3
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-02-10 18:51:29 -0300

    feat: include SearchSection in main layout

--- frontend/src/app/(root)/layout.tsx
+++ frontend/src/app/(root)/layout.tsx
@@ -3,12 +3,16 @@ import { ReactNode } from 'react';
 // Components
 import Header from '@/components/header/header';
 import Footer from '@/components/footer/footer';
+import SearchSection from '@/components/search-section/search-section';
 
 export default function mainLayout({ children }: { children: ReactNode }) {
   return (
-    <main>
+    <main className='h-full w-full'>
       <Header />
+      <SearchSection />
+
       {children}
+
       <Footer />
     </main>
   );
