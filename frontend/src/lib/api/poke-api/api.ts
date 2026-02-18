833b5c77a9157d2f7b61e72c75ca659a1f337f29
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-02-18 18:46:42 -0300

    feat: add generation filter support to getPokemonList

--- frontend/src/lib/api/poke-api/api.ts
+++ frontend/src/lib/api/poke-api/api.ts
@@ -1,14 +1,25 @@
-import { PokeApiListResponse, PokemonEntity } from './types';
+import { POKEMONSBYGENERATION } from '@/constants';
+import { PokeApiResponse, PokemonEntity } from './types';
 
 const POKE_API_BASE_URL = 'https://pokeapi.co/api/v2';
 
 // ─── Fetch Pokemon List ───────────────────────────────────────────────
 export async function getPokemonList(
-  limit: number = 20,
-  offset: number = 0
-): Promise<PokeApiListResponse> {
+  limit = 20,
+  offset = 0,
+  generation: keyof typeof POKEMONSBYGENERATION = 'all'
+): Promise<PokeApiResponse> {
+  const maxOffset = POKEMONSBYGENERATION[generation].end - limit;
+
+  if (offset > maxOffset) {
+    limit = POKEMONSBYGENERATION[generation].end - offset;
+  }
+
   const response = await fetch(
-    `${POKE_API_BASE_URL}/pokemon/?limit=${limit}&offset=${offset}`
+    `${POKE_API_BASE_URL}/pokemon/?limit=${limit}&offset=${offset}`,
+    {
+      next: { revalidate: 60 * 60 },
+    }
   );
 
   if (!response.ok) {
@@ -31,6 +42,18 @@ export async function getPokemonByNameOrId(
   if (!response.ok) {
     throw new Error(
       `Failed to fetch pokemon details: status: ${response.status}, statusText: ${response.statusText}`
+    );
+  }
+
+  return response.json();
+}
+
+export async function getGenerationList(): Promise<PokeApiResponse> {
+  const response = await fetch(`${POKE_API_BASE_URL}/generation`);
+
+  if (!response.ok) {
+    throw new Error(
+      `Failed to fetch generation list: status: ${response.status}, statusText: ${response.statusText}`
     );
   }
 
