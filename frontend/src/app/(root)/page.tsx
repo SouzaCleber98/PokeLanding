2205f9c3b6abfa5fbcf5a876663ad8df26660346
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-02-24 21:10:24 -0300

    refactor: rename Home function to HomePage for consistency

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Carousel from '@/components/carousel/carousel';
 
 import { carouselImages } from '@/constants';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div className='my-5 flex justify-center'>
       <Carousel carouselImages={carouselImages} />
