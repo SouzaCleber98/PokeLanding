4bb6141b2cacb90e2d4b7d32a303edd1ba8935e6
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-02-24 21:10:15 -0300

    refactor: correct function name to MainLayout for consistency

--- frontend/src/app/(root)/layout.tsx
+++ frontend/src/app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import Header from '@/components/header/header';
 import Footer from '@/components/footer/footer';
 import SearchSection from '@/components/search-section/search-section';
 
-export default function mainLayout({ children }: { children: ReactNode }) {
+export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
