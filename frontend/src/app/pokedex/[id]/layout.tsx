7849e89b7b6a54f13c050526fc928a2cf04b4be2
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-02-24 23:10:21 -0300

    feat: implement PokedexLayout component for Pokémon detail view

---
+++ frontend/src/app/pokedex/[id]/layout.tsx
@@ -0,0 +1,51 @@
+import PokedexHero from '@/components/pokemon/pokedex-detail/pokedex-hero';
+import PokedexFooter from '@/components/pokemon/pokedex-detail/pokedex-layout/pokedex-footer';
+import PokedexHeader from '@/components/pokemon/pokedex-detail/pokedex-layout/pokedex-header';
+import PokedexMain from '@/components/pokemon/pokedex-detail/pokedex-layout/pokedex-main';
+import PokedexNavMenu from '@/components/pokemon/pokedex-detail/pokedex-nav-menu';
+
+import {
+  getPokemonByNameOrId,
+  getPokemonSpeciesByNameOrId,
+} from '@/lib/api/poke-api/api';
+import { PokemonEntity, SpeciesInformation } from '@/lib/api/poke-api/types';
+
+type PokedexLayoutProps = {
+  children: React.ReactNode;
+  params: Promise<{ id: string }>;
+};
+
+export default async function PokedexLayout({
+  children,
+  params,
+}: PokedexLayoutProps) {
+  const { id } = await params;
+
+  let pokemonData: PokemonEntity | null = null;
+  let speciesInfo: SpeciesInformation | null = null;
+
+  try {
+    pokemonData = await getPokemonByNameOrId(id);
+    speciesInfo = await getPokemonSpeciesByNameOrId(id);
+  } catch (e) {
+    console.error(e);
+  }
+
+  if (!pokemonData || !speciesInfo) {
+    return <section>Pokemon not found</section>;
+  }
+
+  return (
+    <>
+      <PokedexHeader />
+
+      <PokedexMain mainColor={speciesInfo.color.name}>
+        <PokedexHero pokemonData={pokemonData} speciesData={speciesInfo} />
+        <PokedexNavMenu id={pokemonData.id} />
+
+        {children}
+      </PokedexMain>
+      <PokedexFooter />
+    </>
+  );
+}
