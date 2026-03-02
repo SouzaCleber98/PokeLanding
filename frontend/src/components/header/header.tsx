f309ac81afb8f9bb5035062a1d76c82dd0dc5f1a
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-03-02 15:25:43 -0300

    refactor: update Navbar import path and remove unused import

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,8 +1,6 @@
-import { cn } from '@/lib/utils';
-
 import Image from 'next/image';
 import Link from 'next/link';
-import Navbar from './navbar';
+import Navbar from '../ui/navbar';
 
 export default function Header() {
   return (
