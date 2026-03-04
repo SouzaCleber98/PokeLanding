df14e0ab3916b32a1141d2b4c691ec8c85cae6c1
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-03-04 16:12:49 -0300

    feat: implement search submission with URL query param integration

--- frontend/src/components/search-section/search-input.tsx
+++ frontend/src/components/search-section/search-input.tsx
@@ -1,48 +1,65 @@
+'use client';
+
 import {
   InputGroup,
   InputGroupAddon,
   InputGroupButton,
   InputGroupInput,
 } from '@/components/ui/input-group';
+import useGenerateSearchParams from '@/hooks/useGenerateSearchParams';
 import { cn } from '@/lib/utils';
-
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 
 export default function SearchInput() {
+  const setSearchParams = useGenerateSearchParams();
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const search = formData.get('search')?.toString().trim().toLowerCase();
+
+    if (!search?.length) return;
+    setSearchParams('search', search);
+  };
+
   return (
-    <InputGroup
-      className={cn(
-        'border-red-300 bg-white overflow-hidden',
-        'has-[[data-slot=input-group-control]:focus-visible]:ring-blue-600!'
-      )}
-    >
-      <InputGroupInput
-        className='text-center placeholder:text-gray-400'
-        placeholder='Search Pokémon by name or number...'
-      />
-      <InputGroupAddon>
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
-      </InputGroupAddon>
-      <InputGroupAddon
-        className='border-l-2 border-l-gray-400'
-        align='inline-end'
+    <form onSubmit={handleSubmit}>
+      <InputGroup
+        className={cn(
+          'border-red-300 bg-white overflow-hidden',
+          'has-[[data-slot=input-group-control]:focus-visible]:ring-blue-600!'
+        )}
       >
-        <InputGroupButton
-          size='sm'
-          variant='ghost'
-          className='rounded-full hover:bg-transparent'
+        <InputGroupInput
+          name='search'
+          className='text-center placeholder:text-gray-400'
+          placeholder='Search Pokémon by name or number...'
+        />
+        <InputGroupAddon>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </InputGroupAddon>
+        <InputGroupAddon
+          className='border-l-2 border-l-gray-400'
+          align='inline-end'
         >
-          <span>Search</span>
-          <Image
-            src='/images/pokeball.png'
-            alt='search-button'
-            width={20}
-            height={20}
-          />
-        </InputGroupButton>
-      </InputGroupAddon>
-    </InputGroup>
+          <InputGroupButton
+            size='sm'
+            type='submit'
+            variant='ghost'
+            className='rounded-full hover:bg-transparent'
+          >
+            <span>Search</span>
+            <Image
+              src='/images/pokeball.png'
+              alt='search-button'
+              width={20}
+              height={20}
+            />
+          </InputGroupButton>
+        </InputGroupAddon>
+      </InputGroup>
+    </form>
   );
 }
