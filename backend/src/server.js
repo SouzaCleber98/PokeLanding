cfba4699d8ad0914788799d72a14d4c86968a960
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-04-22 20:11:50 -0300

    refactor: update server port to 4000 and standardize import quotes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
-import app from './app.js';
-const PORT = process.env.PORT || 3000;
+import app from "./app.js";
+const PORT = process.env.PORT || 4000;
 // Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na PORTa ${PORT}`);
