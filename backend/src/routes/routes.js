eda281cf1c27ab6d766da75f3011ceed03286501
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-04-22 20:34:09 -0300

    refactor: clean up routes.js by removing unused route handlers

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,83 +1,25 @@
-import { Router } from 'express';
+import { Router } from "express";
 const routes = new Router();
-import users from '../controllers/UsersController.js';
-import sessions from '../controllers/SessionsController.js';
-import auth from '../middlewares/auth.js';
-import checkId from '../middlewares/checkId.js';
-import contacts from '../controllers/ContactsController.js';
+import users from "../controllers/UsersController.js";
+import sessions from "../controllers/SessionsController.js";
+import auth from "../middlewares/auth.js";
+import checkId from "../middlewares/checkId.js";
+import contacts from "../controllers/ContactsController.js";
 
-routes.get('/', (req, res) => {
-    res.render('index', {
-        title: 'PokeLanding',
-        showSearchbar: false,
-        linkName: 'Pokedex',
-        link: '/pokedex',
-        scripts: [
-            { src: '/assets/js/modal.js', type: 'module' },
-            { src: '/assets/js/header.js' },
-            { src: '/assets/js/cpfMask.js' },
-            { src: '/assets/js/cpfValidate.js' },
-            { src: '/assets/js/cpfListener.js' },
-            { src: '/assets/js/auth.js', type: 'module' },
-            { src: '/assets/js/carousel-auto.js', type: 'module' },
-            { src: '/assets/js/contato.js', type: 'module' },
-            { src: '/assets/js/main.js', type: 'module' }
-        ]
-    });
-});
+routes.post("/sessions", sessions.create);
 
-routes.get('/pokedex', (req, res) => {
-    res.render('pokedex', {
-        title: 'Pokedex',
-        showSearchbar: true,
-        linkName: 'Voltar',
-        link: '/',
-        scripts: [
-            { src: '/assets/js/modal.js', type: 'module' },
-            { src: '/assets/js/header.js' },
-            { src: '/assets/js/cpfMask.js' },
-            { src: '/assets/js/cpfValidate.js' },
-            { src: '/assets/js/cpfListener.js' },
-            { src: '/assets/js/auth.js', type: 'module' },
-            { src: '/assets/js/contato.js', type: 'module' },
-            { src: '/assets/js/pokeSearchLogic.js' },
-            { src: '/assets/js/main.js', type: 'module' }
-        ]
-    });
-});
+routes.post("/users", users.create);
 
-routes.get('/details', (req, res) => {
-    res.render('details', {
-        title: 'Pokedex',
-        showSearchbar: true,
-        link: '/',
-        scripts: [
-            { src: '/assets/js/modal.js', type: 'module' },
-            { src: '/assets/js/header.js' },
-            { src: '/assets/js/cpfMask.js' },
-            { src: '/assets/js/cpfValidate.js' },
-            { src: '/assets/js/cpfListener.js' },
-            { src: '/assets/js/auth.js', type: 'module' },
-            { src: '/assets/js/contato.js', type: 'module' },
-            { src: '/assets/js/pokeSearchLogic.js' }
-        ]
-    });
-});
-
-routes.post('/sessions', (sessions.create));
-
-routes.post('/users', (users.create));
-
-routes.post('/contacts', (contacts.store));
+routes.post("/contacts", contacts.store);
 
 routes.use(auth);
 
-routes.get('/users/me', (users.show));
+routes.get("/users/me", users.show);
 
-routes.param('id', checkId);
+routes.param("id", checkId);
 
-routes.put('/users/:id', (users.update));
+routes.put("/users/:id", users.update);
 
-routes.delete('/users/:id', (users.destroy));
+routes.delete("/users/:id", users.destroy);
 
 export default routes;
