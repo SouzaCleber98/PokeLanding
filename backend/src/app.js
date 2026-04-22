e066c18ecd9ed8dcbf6c4b922172b3b623e635be
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-04-22 20:11:20 -0300

    chore: remove unused config and configure CORS middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,35 +1,24 @@
-import express from 'express';
-import routes from './routes/routes.js';
-import database from './database/index.js';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
-
-// Configuração do caminho para o diretório atual
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import express from "express";
+import routes from "./routes/routes.js";
+import database from "./database/index.js";
+import dotenv from "dotenv";
+import cors from "./middlewares/cors.js";
 
 class App {
   constructor() {
     dotenv.config();
     this.server = express();
-    this.config();
     this.middlewares();
     this.routes();
 
     database.init();
   }
 
-  config() {
-    // Configurações
-    this.server.set('views', path.join(__dirname, '..', '..', 'frontend', 'views'));
-    this.server.set('view engine', 'ejs');
-    this.server.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'public')));
-  }
-
   middlewares() {
     // Middlewares
     this.server.use(express.urlencoded({ extended: true }));
     this.server.use(express.json());
+    this.server.use(cors);
   }
 
   routes() {
