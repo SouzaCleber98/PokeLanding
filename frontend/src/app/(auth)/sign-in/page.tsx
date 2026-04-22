faf3c1f06961a314a41610c329e2cd8e6a82884e
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-04-22 20:32:43 -0300

    feat: add onSuccess handler to AuthForm in SignInPage and SignUpPage for session management

--- frontend/src/app/(auth)/sign-in/page.tsx
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,17 @@
+'use client';
+
 import AuthForm from '@/components/forms/auth-form';
 export default function SignInPage() {
-  return <AuthForm type='SIGN_IN' />;
+  return (
+    <AuthForm
+      type='SIGN_IN'
+      onSuccess={(data) =>
+        fetch('http://localhost:4000/sessions', {
+          method: 'POST',
+          headers: { 'Content-type': 'application/json' },
+          body: JSON.stringify({ ...data }),
+        })
+      }
+    />
+  );
 }
--- frontend/src/app/(auth)/sign-up/page.tsx
+++ frontend/src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,18 @@
+'use client';
+
 import AuthForm from '@/components/forms/auth-form';
 
 export default function SignUpPage() {
-  return <AuthForm type='SIGN_UP' />;
+  return (
+    <AuthForm
+      type='SIGN_UP'
+      onSuccess={(data) =>
+        fetch('http://localhost:4000/users', {
+          method: 'POST',
+          headers: { 'Content-type': 'application/json' },
+          body: JSON.stringify({ ...data }),
+        })
+      }
+    />
+  );
 }
