7200c2a2dbba58c3e78da039c4758390cc11b00f
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-06-04 18:05:05 -0300

    refactor: remove legacy backend and old frontend code

--- backend/src/server.js
+++
@@ -1,6 +0,0 @@
-import app from "./app.js";
-const PORT = process.env.PORT || 4000;
-// Inicia o servidor
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na PORTa ${PORT}`);
-});
--- old-frontend/public/assets/js/main.js
+++
@@ -1,1 +0,0 @@
-import UserInfoButtons from "./UserInfoButtons.js";
