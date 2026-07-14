f4078e42a75753baac675a75bcad3cefab104b7e
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-07-14 15:59:51 -0300

    chore: add updateUser and deleteUser functions with TODOs for implementation

---
+++ src/actions/user.ts
@@ -0,0 +1,8 @@
+'use server';
+
+export async function updateUser() {
+  // TODO: Implement the updateUser function to handle updating user information in the database.
+}
+export async function deleteUser() {
+  // TODO: Implement the deleteUser function to handle deleting a user from the database.
+}
