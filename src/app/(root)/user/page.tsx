c828c88721a45ac0873c1a02de33612080bb99be
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-08-10 12:16:16 -0300

    feat: implement user page to display user information

--- src/app/(root)/user/page.tsx
+++ src/app/(root)/user/page.tsx
@@ -1,4 +1,14 @@
-export default function UserPage() {
+import UserInfoSection from '@/components/user-info-section/user-info-section';
+import { getUserFromSession } from '@/lib/auth/session';
+import { cookies } from 'next/headers';
+import { unauthorized } from 'next/navigation';
+
+export default async function UserPage() {
   // TODO: Implement the user page to display user information and allow updates or deletion of the user account.
-  return <>hello</>;
+  const session = await getUserFromSession(await cookies());
+
+  if (!session) unauthorized();
+
+  const { username, email } = session;
+  return <UserInfoSection username={username} email={email} />;
 }
