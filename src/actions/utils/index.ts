e1df1df8b5e1dda14adce9dd5f40dc835553b08b
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-08-13 16:03:34 -0300

    chore: implement handleDelete function for user deletion and redirection

--- src/actions/utils/index.ts
+++ src/actions/utils/index.ts
@@ -2,8 +2,14 @@
 
 import { redirect } from 'next/navigation';
 import { logOut } from '../auth';
+import { deleteUser } from '../user';
 
 export async function handleLogout() {
   await logOut();
+  redirect('/');
+}
+
+export async function handleDelete() {
+  await deleteUser();
   redirect('/');
 }
