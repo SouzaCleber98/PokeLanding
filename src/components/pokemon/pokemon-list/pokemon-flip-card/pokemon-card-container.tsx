5ec8b135a8396edc56082eab32bb78adc6fefacf
CleberSouza <souzacleber.cs.cs@gmail.com>
2026-08-18 20:52:53 -0300

    refactor: fetch card data on server
    Replace client-side state and effect with server-side data fetching in the
    Pokemon card container.

--- src/components/pokemon/pokemon-list/pokemon-flip-card/pokemon-card-container.tsx
+++ src/components/pokemon/pokemon-list/pokemon-flip-card/pokemon-card-container.tsx
@@ -1,31 +1,16 @@
-'use client';
-
 import { getPokemonByNameOrId } from '@/lib/api/poke-api/api';
 import PokemonDataProvider from '../../../../context/pokemon-data-context/pokemon-data-provider';
 import PokemonFlipCard from './pokemon-flip-card';
-import { useEffect, useState } from 'react';
 import { PokemonEntity } from '@/lib/api/poke-api/types/types';
 
 type PokemonCardContainerProp = {
   pokemonName: string;
 };
 
-export default function PokemonCardContainer({
+export default async function PokemonCardContainer({
   pokemonName,
 }: PokemonCardContainerProp) {
-  const [pokemonData, setPokemonData] = useState<PokemonEntity>();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setPokemonData(await getPokemonByNameOrId(pokemonName));
-    };
-    fetchData();
-  }, []);
-
-  //TODO: arrumar loading state, talvez com skeleton loader
-  if (!pokemonData) {
-    return <div>Carregando...</div>;
-  }
+  const pokemonData = await getPokemonByNameOrId(pokemonName);
 
   return (
     <PokemonDataProvider pokemonData={pokemonData}>
